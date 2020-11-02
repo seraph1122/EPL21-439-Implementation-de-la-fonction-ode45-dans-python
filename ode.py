42ae661199292361ebb6d2e777fb05275b49ed55
Louis <l.declercq@student.uclouvain.be>
2020-11-02 18:25:42 +0100

    ode cleanup

--- ode.py
+++ ode.py
@@ -20,6 +20,7 @@ def ode45(odefun,tspan,y0,options=[],varagin=[]):
   
     #Outputs
     FcnHandlesUsed=callable(odefun)
+    output_sol=0 #Temp
     ''' 100 - 110
     TODO : Outputs
     '''
@@ -30,6 +31,8 @@ def ode45(odefun,tspan,y0,options=[],varagin=[]):
     ''' 118 - 144
     TODO : Handle Outputs
     '''
+    refine=4 #Temp
+    s=np.array(range(1,refine))/refine #Temp
     
     haveEventFcn,eventFcn,eventArgs,valt,teout,yeout,ieout=0,None,None,None,None,None,None #Temp
     
@@ -42,35 +45,41 @@ def ode45(odefun,tspan,y0,options=[],varagin=[]):
     '''
     
     nonNegative=0 #Temp
+    output_ty=1 #Temp
     
     ''' 167 - 172
     TODO : Non-negative solution
     '''
     
-    
-    
     t=t0
     y=y0
     
-    yout=[]
-    tout=[]
-    error=[0]
-    output_ty=1
-    
-    yout.append(y)
-    tout.append(t)
-    
-    ynew=np.zeros(neq)
-    
-    refine=4
-    s=np.array(range(1,refine))/refine
-    
-    hmin=16*np.finfo(float(t)).eps
-    absh=hmax
-    
+    #Memory Allocation
     nout=1
+    yout=np.array([],dtype=dataType)
+    tout=np.array([],dtype=dataType)
+    nargout=2 #TODO
+    if nargout > 0:
+        if output_sol:
+            chunk = min(max(100,50*refine), refine+math.floor(math.pow(2,11)/neq))
+            tout = np.zeros((1,chunk),dtype=dataType)
+            yout = np.zeros((neq,chunk),dtype=dataType)
+            #TODO f3d
+        else:
+            if ntspan > 2:
+              tout = np.zeros((1,ntspan),dtype=dataType)
+              yout = np.zeros((neq,ntspan),dtype=dataType)
+            else:
+              chunk = min(max(100,50*refine), refine+math.floor(math.pow(2,13)/neq))
+              tout = np.zeros((1,chunk),dtype=dataType)
+              yout = np.zeros((neq,chunk),dtype=dataType)
+        nout = 1
+        tout[nout-1] = t
+        yout[:,nout-1] = y.copy()
     
-    pow = 1/5
+    
+    #Initialize method parameters
+    power = 1/5
     A = np.array([1/5, 3/10, 4/5, 8/9, 1, 1])
     B = np.array([[1/5,         3/40,    44/45,   19372/6561,      9017/3168,       35/384    ],
         [0,           9/40,    -56/15,  -25360/2187,     -355/33,         0           ],
@@ -80,11 +89,37 @@ def ode45(odefun,tspan,y0,options=[],varagin=[]):
         [0,           0,       0,       0,               0,               11/84       ],
         [0,           0,       0,       0,               0,               0           ]])
     E = np.array([[71/57600], [0], [-71/16695], [71/1920], [-17253/339200], [22/525], [-1/40]])
-    f=np.zeros((neq,7))
+    f=np.zeros((neq,7),dtype=dataType)
+    hmin=16*np.finfo(float(t)).eps
     
-    for iy0 in range(neq):
-        f[iy0,0]=f0[iy0]
     
+    if len(htry)==0:
+        absh = min(hmax, htspan)
+        if normcontrol:
+            if len(normy)==0:
+                rh=(np.linalg.norm(f0)/ threshold)/ (0.8 * math.pow(rtol,power))
+            else:
+                rh=(np.linalg.norm(f0)/ max(threshold,max(normy)))/ (0.8 * math.pow(rtol,power))
+        else:
+            rh=(np.linalg.norm(f0 / max(max(np.abs(y)),threshold),np.inf))/ (0.8 * math.pow(rtol,power))
+            
+        if (absh * rh) > 1:
+            absh =1/rh
+        absh = max(absh,hmin)
+    else:
+        if len(htry)==0:
+            absh=min(absh,hmin)
+        else:
+            absh=min(absh,max(hmin,max(htry)))
+    
+    f[:,0]=f0
+    
+    ''' 232 - 236
+    TODO : Init ouput function
+    '''
+    
+    ynew=np.zeros(neq)
+        
     
     done=False
     while not done:
@@ -102,7 +137,6 @@ def ode45(odefun,tspan,y0,options=[],varagin=[]):
             hA = h * A
             hB = h * B
             
-            
             for iy0 in range(neq):
                 f[iy0,1]=odefun(t+hA[0],y[iy0]+np.dot(f[iy0],hB[:,0]))
                 f[iy0,2]=odefun(t+hA[1],y[iy0]+np.dot(f[iy0],hB[:,1]))
@@ -111,7 +145,6 @@ def ode45(odefun,tspan,y0,options=[],varagin=[]):
                 f[iy0,5]=odefun(t+hA[4],y[iy0]+np.dot(f[iy0],hB[:,4]))
             
             
-        
             tnew = t + hA[5]
             if done:
               tnew = tfinal
@@ -121,77 +154,92 @@ def ode45(odefun,tspan,y0,options=[],varagin=[]):
                 ynew[iy0] = y[iy0] + np.dot(f[iy0],hB[:,5])
             for iy0 in range(neq):
                 f[iy0,6]=odefun(tnew,ynew[iy0])
+            nfevals=nfevals+6
             
+            
+            NNrejectStep = False
+            if normcontrol:
+                ''' 280 - 289
+                TODO : Estimate Error
+                '''
+            else:
+                denom=np.linalg.norm(np.maximum(np.maximum(np.abs(y),np.abs(ynew)),threshold),np.inf)
+                err=absh*np.linalg.norm(np.matmul(f,E))/denom
+                ''' 292 - 298
+                TODO : Non-negative
+                '''
 
-            denom=np.linalg.norm(np.maximum(np.maximum(np.abs(y),np.abs(ynew)),threshold),np.inf)
-            err=absh*np.linalg.norm(np.matmul(f,E))/denom
-            error.append(err)
+            ''' 305 - 348
+            TODO : Failed step
+            '''
+            NNreset_f7 = False #Temp
+                
+            break
+        nsteps+=1
+        
+        ''' 351 - 371
+        TODO : Event Function
+        '''
+        
+        ''' 375 - 385
+        TODO : Failed step
+        '''
+        
+        if output_ty or haveOutputFcn:
+            ''' 389 - 392
+            TODO : Solver Steps
+            '''
             
-            h = tdir * absh
+            #Refinement
+            tref=t+(tnew-t)*s
+            nout_new=refine
+            tout_new=tref.copy()
+            tout_new=np.append(tout_new,tnew)
+            yout_new=np.transpose(ntrp.ntrp45(tref,t,y,h,f))
+            yout_new=np.append(yout_new,np.array([ynew]),axis=0)
+            yout_new=np.transpose(yout_new)
+            
+            ''' 398 - 420
+            TODO : Requested Points
+            '''
+            
+            if nout_new > 0:
+                if output_ty:
+                    oldnout=nout
+                    nout=nout+nout_new
+                    if nout>len(tout[0]):
+                        talloc=np.zeros((1,chunk),dtype=dataType)
+                        tout=np.append(tout,talloc)
+                        yalloc=np.zeros((neq,chunk),dtype=dataType)
+                        yout=np.append(yout,yalloc,axis=0)
+                    for i in range(oldnout,nout):
+                        tout[0,i] = tout_new[i-oldnout]
+                        yout[:,i] = yout_new[:,i-oldnout]
+                    
+                    ''' 434 - 439
+                    TODO : haveOutputFcn
+                    '''
+        
+        if done:
             break
         
-        #Refinement
-        tref=t+(tnew-t)*s
-        
-        nout_new=refine
-        tout_new=tref.copy()
-        tout_new=np.append(tout_new,tnew)
-        yout_new=np.transpose(ntrp.ntrp45(tref,t,y,h,f))
-        
-        
-        while nex <= ntspan:
-            if tdir * (tnew - tspan[nex-1]) < 0:
-                break
-            nout_new = nout_new + 1
-            
-            np.append(tout_new,tspan[nex-1])
-            #not done
-            nex = nex + 1
-        
-        
-#        if nout_new > 0:
-#            if output_ty:
-#                oldnout = nout
-#                nout = nout + nout_new
-#                for i in range(oldnout,nout):
-#                    tout[i] = tout_new[i-oldnout]
-#                    yout[:,i] = yout_new[i-oldnout]
-        
-        
         if True:
-            temp = math.pow(1.25*(err/rtol),pow)
+            temp = math.pow(1.25*(err/rtol),power)
             if temp > 0.2:
                 absh=absh/temp
             else:
                 absh=0.5*absh
-            
         
-#        if nofailed
-#            % Note that absh may shrink by 0.8, and that err may be 0.
-#            temp = 1.25*(err/rtol)^pow;
-#            if temp > 0.2
-#              absh = absh / temp;
-#            else
-#              absh = 5.0*absh;
-#            end
-#          end
                 
         
-        if done:
-            break
+        
             
         t=tnew
         y=ynew.copy()
-        for i in yout_new:
-            yout.append(i)
-        for i in tout_new:
-            tout.append(i)
-        yout.append(y)
         f[:,0]=f[:,6]
         
         nsteps+=1
-    
-    return OdeResult.OdeResult(solver_name='ode45',odefun=odefun,t=tout,y=np.array(yout),err=error,nsteps=nsteps)
+    return OdeResult.OdeResult(solver_name='ode45',odefun=odefun,t=tout[0,0:nout],y=yout[:,0:nout],nsteps=nsteps)
     
 
     
