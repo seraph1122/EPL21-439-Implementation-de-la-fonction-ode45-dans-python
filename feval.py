02beb80d52b1454f859764fc19a352f34c0697d6
Louis <l.declercq@student.uclouvain.be>
2021-02-16 14:26:57 +0100

    feval

--- feval.py
+++ feval.py
@@ -29,34 +29,4 @@ def feval(fun,t,y,extra):
              raise Exception("{}:feval:OtherException".format(sys.exc_info()[0]))
     
     return np.array(result)
-    
-    
-#    if extra==[]:
-#        try:
-#            result=fun(t,y)
-#            if len(result)==0:
-#                result = np.zeros(len(y))
-#                for i in range(len(y)):
-#                    result[i]=fun(t,y[i])
-#        except TypeError:
-#            result = np.zeros(len(y))
-#            for i in range(len(y)):
-#                result[i]=fun(t,y[i])
-#        except:
-#            print("Unexpected error:", sys.exc_info()[0])
-#    else:
-#        try:
-#            result=fun(t,y,extra)
-#            if len(result)==0:
-#                result = np.zeros(len(y))
-#                for i in range(len(y)):
-#                    result[i]=fun(t,y[i],extra)
-#        except TypeError:
-#            result = np.zeros(len(y))
-#            for i in range(len(y)):
-#                result[i]=fun(t,y[i],extra)
-#        except:
-#            print("Unexpected error:", sys.exc_info()[0])
-#    
-#    return np.array(result)
-#    
+      
